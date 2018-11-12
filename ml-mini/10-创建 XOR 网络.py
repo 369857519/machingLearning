87c23959c4d15abf10276d1e3760d6984128b0d3
qilianshan <qilianshan@xiaomi.com>
2018-11-12 17:22:53 +0800

    fi
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- ml-mini/10-创建 XOR 网络.py
+++ ml-mini/10-创建 XOR 网络.py
@@ -13,12 +13,13 @@
 
 import numpy as np
 
+
 class Perceptron:
     """
     This class models an artificial neuron with step activation function.
     """
 
-    def __init__(self, weights = np.array([1]), threshold = 0):
+    def __init__(self, weights=np.array([1]), threshold=0):
         """
         Initialize weights and threshold based on input arguments. Note that no
         type-checking is being performed here for simplicity.
@@ -26,28 +27,29 @@ class Perceptron:
         self.weights = weights
         self.threshold = threshold
 
-
     def activate(self, values):
         """
         Takes in @param values, a list of numbers equal to length of weights.
         @return the output of a threshold perceptron with given inputs based on
         perceptron weights and threshold.
         """
-               
+
         # First calculate the strength with which the perceptron fires
-        strength = np.dot(values,self.weights)
-        
+        strength = np.dot(values, self.weights)
+
         # Then return 0 or 1 depending on strength compared to threshold  
         return int(strength > self.threshold)
+
 
 # -and+or
 # Part 1: Set up the perceptron network
 Network = [
     # input layer, declare input layer perceptrons here
-    [[0.5,0.5,0.75],[0.5,0.5,0.25]],
+    [[0.5, 0.5, 0.75], [0.5, 0.5, 0.25]],
     # output node, declare output layer perceptron here
-    [[-1,1]]
+    [[-1, 1]]
 ]
+
 
 # Part 2: Define a procedure to compute the output of the network, given inputs
 def EvalNetwork(inputValues, Network):
@@ -56,12 +58,18 @@ def EvalNetwork(inputValues, Network):
     that specifies a perceptron network. @return the output of the Network for
     the given set of inputs.
     """
-    for i,v in enumerate(Network):
-        for j,perceptron in enumerate(v):
-
-    
+    OutputValue=1;
+    for i, v in enumerate(Network):
+        # 操作第N层
+        res=[1]*len()
+        newInput=[]
+        for j, perceptron in enumerate(v):
+            perceptron=Perceptron(v);
+            newInput[i]=perceptron.activate(inputValues)
+        inputValues=newInput;
     # YOUR CODE HERE
-
+    if(len(newInput) == 1):
+        OutputValue=newInput[0];
     # Be sure your output value is a single number
     return OutputValue
 
@@ -70,10 +78,11 @@ def test():
     """
     A few tests to make sure that the perceptron class performs as expected.
     """
-    print "0 XOR 0 = 0?:", EvalNetwork(np.array([0,0]), Network)
-    print "0 XOR 1 = 1?:", EvalNetwork(np.array([0,1]), Network)
-    print "1 XOR 0 = 1?:", EvalNetwork(np.array([1,0]), Network)
-    print "1 XOR 1 = 0?:", EvalNetwork(np.array([1,1]), Network)
+    print("0 XOR 0 = 0?:", EvalNetwork(np.array([0, 0]), Network))
+    print("0 XOR 1 = 1?:", EvalNetwork(np.array([0, 1]), Network))
+    print("1 XOR 0 = 1?:", EvalNetwork(np.array([1, 0]), Network))
+    print("1 XOR 1 = 0?:", EvalNetwork(np.array([1, 1]), Network))
+
 
 if __name__ == "__main__":
     test()
