35c8626ba8d63f8478cb07632eef5e3118ab1694
Dante <369857519@qq.com>
2019-06-03 09:09:41 +0800

    one hot
    Signed-off-by: Dante <369857519@qq.com>

--- mnist/index.py
+++ mnist/index.py
@@ -1,19 +1,35 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-minst=tf.keras.datasets.mnist
-(x_train,y_train),(x_test,y_test)=minst.load_data('mnist\mnist.npz')
-# 60000张图片 60000个标签
-print(x_train.shape,y_train.shape)
-# 测试集 10000 10000
-print(x_test.shape,y_test.shape)
+from keras.datasets import mnist
+from keras.utils import np_utils
 
-#观察数据
-fig=plt.figure()
-for i in range(15):
-    plt.subplot(3,5,i+1)
-    plt.tight_layout()
-    plt.imshow(x_train[i],cmap='Greys')
-    plt.title("Label:{}".format(y_train[i]))
-    plt.xticks([])
-    plt.yticks([])
+(x_train, y_train), (x_test, y_test) = mnist.load_data('mnist\mnist.npz')
+# 60000张图片 60000个标签
+print(x_train.shape, y_train.shape)
+# 测试集 10000 10000
+print(x_test.shape, y_test.shape)
+
+# 观察数据
+X_train = x_train.reshape(60000, 784)
+X_test = x_test.reshape(10000, 784)
+print(X_train.shape, type(X_train))
+print(X_test.shape, type(y_test))
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train/=255
+X_test/=255
+
+# 观察标签
+
+import numpy as np
+import matplotlib.pyplot as plt
+label,count=np.unique(y_train,return_counts=True)
+print(label,count)
+
+#one hot
+n_classes=10
+print(y_train.shape)
+Y_train=np_utils.to_categorical(y_train,n_classes)
+print(Y_train.shape)
+Y_test=np_utils.to_categorical(y_test,n_classes)
 
